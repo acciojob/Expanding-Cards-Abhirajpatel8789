0ab57738e4f2bf60fb229e202c06cd84a5b305e0
Abhirajpatel8789 <122907724+Abhirajpatel8789@users.noreply.github.com>
2026-03-26 11:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let panels = document.querySelectorAll('.panel')
+
+let currActive = document.querySelector('.active')
+for(let p of panels){
+	p.addEventListener('click',() => {
+		if(!p.classList.contains('active')){
+			currActive.classList.remove('active');
+			p.classList.add('active')
+			currActive = p;
+		}
+	})
+}
